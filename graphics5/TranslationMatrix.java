a083c44ad38029706bc362ee7fbe65ae52536c8f
jedrek <jedrek@fijalkowscy.com>
2016-07-04 14:50:52 +0200

    fix

--- graphics5/TranslationMatrix.java
+++ graphics5/TranslationMatrix.java
@@ -3,7 +3,7 @@ package graphics5;
 public class TranslationMatrix extends Matrix4x4{
         public TranslationMatrix()
         {
-            super.set_m(0,1, 1f);
+            super.set_m(0,0, 1f);
             super.set_m(1,1, 1f);
             super.set_m(2,2, 1f);
             super.set_m(3,3, 1f);
